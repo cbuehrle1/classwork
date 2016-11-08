5c6d5a48be06d4f5ceb375eebfd092f98002808f
Chad Buehrle <chad.buehrle@gmail.com>
2016-11-08 11:47:48 -0600

    update3

---
+++ little-js-labs/loops/lab-09.js
@@ -0,0 +1,25 @@
+var names = ['Fred', 'Wilma', 'Barney', 'Betty'];
+var eNames = [];
+
+//loop goes here
+
+var i = 0
+
+while (i < names.length)
+{
+  if(names[i].indexOf("e") != -1)
+  {
+    eNames.push(names[i]);
+  };
+  i++;
+}
+
+
+console.log(eNames);
+
+/*
+Write a while loop to loop over the array and use the loop to check each name to see if it contains the letter 'e'. If it does, add it to the eNames array.
+Note: you might want to remind yourself of how the `indexOf` function works on string.
+Expected Output:
+[ 'Fred', 'Barney', 'Betty' ]
+*/
