079d9597511c8e25bf35ca832cb7cd8afe660cce
Chad Buehrle <chad.buehrle@gmail.com>
2016-11-15 14:35:38 -0600

    day12update

--- little-js-labs/day-11-Scorecard/scorecard.js
+++ little-js-labs/day-11-Scorecard/scorecard.js
@@ -6,5 +6,8 @@ var lines = fileImport.split("\n");
 
 for(var i = 1; i < lines.length - 1; i++){
   var indivArray = lines[i].split(",");
-  console.log(indivArray[3] + ", " + indivArray[32]);
+  if(indivArray[32] === "NULL"){
+  } else {
+    console.log(indivArray[3] + ", " + indivArray[32]);
+  }
 }
