c0ada7cc2140c67852616f4cfb75eac4ba89f094
Chad Buehrle <chad.buehrle@gmail.com>
2016-11-17 00:14:53 -0600

    office365

---
+++ little-js-labs/day-13-office-365/index.js
@@ -0,0 +1,24 @@
+var fs = require("fs");
+
+fs.readFile("Import_User_Sample_en.csv", "utf-8", function(err, data){
+
+  var parsed = {
+    UserNames: []
+  }
+
+  var lines = data.split("\r\n");
+
+  for(var i = 1; i < lines.length; i++){
+    var indivArr = lines[i].split(",");
+
+    var userData = {
+      UserName: indivArr[0],
+      DisplayName: indivArr[3]
+    };
+
+    parsed.UserNames.push(userData)
+
+  }
+
+  console.log(parsed.UserNames);
+  });
