a7c48e3e6673cdf9c1d21fcef313074c37b1027a
Chad Buehrle <chad.buehrle@gmail.com>
2016-11-29 13:36:57 -0600

    updated2

--- little-web-assignments/day-21-DOM/day-21-multi-toggle-on-click/script.js
+++ little-web-assignments/day-21-DOM/day-21-multi-toggle-on-click/script.js
@@ -1,24 +1,8 @@
-var buttons = document.querySelectorAll(".bubble")
+var button = document.querySelector(".container")
 
 
-buttons[0].addEventListener("click", function(){
-  buttons[0].classList.toggle("on");
-});
-buttons[1].addEventListener("click", function(){
-  buttons[1].classList.toggle("on");
-});
-buttons[2].addEventListener("click", function(){
-  buttons[2].classList.toggle("on");
-});
-buttons[3].addEventListener("click", function(){
-  buttons[3].classList.toggle("on");
-});
-buttons[4].addEventListener("click", function(){
-  buttons[4].classList.toggle("on");
-});
-buttons[5].addEventListener("click", function(){
-  buttons[5].classList.toggle("on");
-});
-buttons[6].addEventListener("click", function(){
-  buttons[6].classList.toggle("on");
+button.addEventListener("click", function(evt){
+  if (evt.target.tagName === 'DIV') {
+    evt.target.classList.toggle("on");
+  }
 });
