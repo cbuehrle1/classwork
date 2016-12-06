4a0046a5be0937d197086d87465f08904ca0840f
Chad Buehrle <chad.buehrle@gmail.com>
2016-12-05 21:11:28 -0600

    update3

--- little-web-assignments/day-26-multi-step-form/script.js
+++ little-web-assignments/day-26-multi-step-form/script.js
@@ -12,13 +12,28 @@ var count = 0;
 
 pageArr[0].style.display = "block";
 
+function showHideButtons(){
+  if (count === 3){
+    submitB.style.display = "inline";
+    nextB.style.display = "none";
+  } else if (count === 0){
+    prevB.style.display = "none";
+    nextB.style.display = "inline";
+  } else {
+    prevB.style.display = "inline";
+    nextB.style.display = "inline";
+    submitB.style.display = "none";
+  }
+}
+
 nextB.addEventListener("click", function(){
 
   if (count < 3){
     count = count + 1;
+    pageArr[count - 1].style.display = "none";
     pageArr[count].style.display = "block";
+    showHideButtons();
   }
-
 
 });
 
@@ -26,7 +41,9 @@ prevB.addEventListener("click", function(){
 
   if (count > 0){
     count = count - 1;
+    pageArr[count + 1].style.display = "none";
     pageArr[count].style.display = "block";
+    showHideButtons();
   }
-  
+
 });
