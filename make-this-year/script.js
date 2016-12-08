fc83c547d2c401998a99c35a8a42b07860c80f37
Chad Buehrle <chad.buehrle@gmail.com>
2016-12-08 16:49:32 -0600

    js added

---
+++ make-this-year/script.js
@@ -0,0 +1,41 @@
+var purpleDiv = document.querySelector(".purple-header");
+
+var textThatScrolls = ["an Apple Watch app", "a video course", "a podcast", "a blog", "a personal website", "an iPhone app", "an email course", "an Ebook", "an e-commerce site", "a newsletter"]
+
+var counter = 0;
+
+var i = 0;
+
+var direction = "forward";
+
+
+setInterval(function(){
+
+  if (direction === "forward" && counter < textThatScrolls[i].length){
+    purpleDiv.textContent = textThatScrolls[i].slice(0, counter)
+    counter++;
+  } else if (direction === "forward" && counter === textThatScrolls[i].length){
+    direction = "backword"
+    purpleDiv.textContent = textThatScrolls[i].slice(0, counter);
+    counter = counter - 1;
+  } else if (direction === "backword" && counter < textThatScrolls[i].length && counter >= 1){
+    counter = counter - 1;
+    purpleDiv.textContent = textThatScrolls[i].slice(0, counter);
+  } else if (direction === "backword" && counter === 0){
+    purpleDiv.textContent = textThatScrolls[i].slice(0, counter);
+    direction = "forward";
+
+    console.log(textThatScrolls[i])
+
+    if (i === textThatScrolls.length - 1){
+      i = 0;
+    } else {
+      i = i + 1;
+    }
+
+    console.log(textThatScrolls[i])
+
+  }
+
+
+}, 200)
