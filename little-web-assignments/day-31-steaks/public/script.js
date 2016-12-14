e6f8630178aa5fcc93d251a7fda91c81674cb0a4
Chad Buehrle <chad.buehrle@gmail.com>
2016-12-14 10:32:07 -0600

    added mustache

--- little-web-assignments/day-31-steaks/public/script.js
+++ little-web-assignments/day-31-steaks/public/script.js
@@ -7,24 +7,16 @@
 
   promise.done(function(data){
 
+    var html = ""
+
     for(var i = 0; i < data.levels.length; i++){
       var level = data.levels[i];
-      var li = document.createElement("li");
-      var h1 = document.createElement("h1");
-      var p = document.createElement("p");
-      var img = document.createElement("img");
-
-      h1.textContent = level.name
-      p.textContent = level.description
-      img.src = level.imageUrl
-
-      steakTemps.appendChild(li);
-      li.appendChild(h1);
-      li.appendChild(p);
-      li.appendChild(img);
-
+      
+      var output = Mustache.render('<li><h1>{{name}}</h1><p>{{description}}</p><img src="{{imageUrl}}"/></li>', level);
+      html = html + output;
     }
-    
+    steakTemps.innerHTML = html;
+
   });
 
 })()
