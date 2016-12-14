78cf348b97796152caed8d32eef739c1e9d49c75
Chad Buehrle <chad.buehrle@gmail.com>
2016-12-13 18:53:49 -0600

    cleaning up

--- little-web-assignments/day-29-repeat/script.js
+++ little-web-assignments/day-29-repeat/script.js
@@ -4,15 +4,18 @@ var oneB = document.querySelector(".one");
 var twoB = document.querySelector(".two");
 var coolDiv = document.querySelector(".target");
 
+
+
 oneB.addEventListener("click", function(){
-  changeIt();
+  changeIt("change it", "on", "other-on");
 });
 
 twoB.addEventListener("click", function(){
-  changeIt();
+  changeIt("change it different", "other-on", "on");
 });
 
-function changeIt(){
-  coolDiv.textContent = "I changed it"
-  coolDiv.classList.add("on");
+function changeIt(message, style, style1){
+  coolDiv.textContent = message
+  coolDiv.classList.add(style);
+  coolDiv.classList.remove(style1);
 }
