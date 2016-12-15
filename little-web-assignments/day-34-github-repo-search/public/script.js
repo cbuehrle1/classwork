7d568b0fbefa60d4c957886c7383a52b8163c037
Chad Buehrle <chad.buehrle@gmail.com>
2016-12-15 16:36:27 -0600

    part 2 done

--- little-web-assignments/day-34-github-repo-search/public/script.js
+++ little-web-assignments/day-34-github-repo-search/public/script.js
@@ -2,31 +2,69 @@
   var display = document.querySelector(".display");
   var clientInput = document.querySelector(".search-input");
   var template = document.querySelector("#template");
+  var pageCount = document.querySelector(".page-counter");
+  var nextB = document.querySelector(".next-page");
+  var prevB = document.querySelector(".back-page");
+  var pageNav = document.querySelector(".page-nav")
+
+  var page = 1;
+
+  var totalCount;
+
+  function getData (){
+
+    var searchInput = clientInput.value + "&page=" + page;
+
+    var promise = $.ajax({
+      url: "https://api.github.com/search/repositories?q=" + searchInput
+    })
+
+    promise.done(function(data){
+
+      display.innerHTML = ""
+
+      for (var i = 0; i < data.items.length; i++){
+        var item = data.items[i];
+        var templateHtml = template.innerHTML;
+        var output = Mustache.render(templateHtml, item);
+        display.innerHTML = display.innerHTML + output;
+      }
+
+      totalCount = data.total_count;
+      pageCount.innerText = page;
+      pageNav.style.display = "block"
+
+    });
+
+  }
 
   clientInput.addEventListener("keyup", function(evt){
 
     if (evt.keyCode === 13){
+      getData();
+      prevB.classList.add("grey");
+    }
 
-      var searchInput = clientInput.value;
+  });
 
-      var promise = $.ajax({
-        url: "https://api.github.com/search/repositories?q=" + searchInput
-      })
+  nextB.addEventListener("click", function(){
 
-      promise.done(function(data){
-        
-        display.innerHTML = ""
+    if (page < (totalCount / 30)){
+      prevB.classList.remove("grey");
+      page++;
+      getData();
+    } else if (page >= (totalCount / 30)){
+      nextB.classList.add("grey")
+    }
 
-        for (var i = 0; i < data.items.length; i++){
-          var item = data.items[i];
-          var templateHtml = template.innerHTML;
-          var output = Mustache.render(templateHtml, item);
-          display.innerHTML = display.innerHTML + output;
-        }
+  });
 
+  prevB.addEventListener("click", function(){
 
-      });
-
+    if (page > 1){
+      nextB.classList.remove("grey");
+      page--;
+      getData();
     }
 
   });
