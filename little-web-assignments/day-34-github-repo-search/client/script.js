d5ac228cec996f427ea17a5ae9e44ac286990c31
Chad Buehrle <chad.buehrle@gmail.com>
2016-12-19 13:49:46 -0600

    hello

--- little-web-assignments/day-34-github-repo-search/client/script.js
+++ little-web-assignments/day-34-github-repo-search/client/script.js
@@ -1,31 +1,29 @@
-"use strict";
-
-(function () {
+(() => {
   var display = document.querySelector(".display");
   var clientInput = document.querySelector(".search-input");
   var template = document.querySelector("#template");
   var pageCount = document.querySelector(".page-counter");
   var nextB = document.querySelector(".next-page");
   var prevB = document.querySelector(".back-page");
-  var pageNav = document.querySelector(".page-nav");
+  var pageNav = document.querySelector(".page-nav")
 
   var page = 1;
 
   var totalCount;
 
-  function getData() {
+  function getData (){
 
     var searchInput = clientInput.value + "&page=" + page;
 
     var promise = $.ajax({
       url: "https://api.github.com/search/repositories?q=" + searchInput
-    });
+    })
 
-    promise.done(function (data) {
+    promise.done((data) => {
 
-      display.innerHTML = "";
+      display.innerHTML = ""
 
-      data.items.forEach(function (item) {
+      data.items.forEach((item) => {
         var templateHtml = template.innerHTML;
         var output = Mustache.render(templateHtml, item);
         display.innerHTML = display.innerHTML + output;
@@ -33,39 +31,44 @@
 
       totalCount = data.total_count;
       pageCount.innerText = page;
-      pageNav.style.display = "block";
+      pageNav.style.display = "block"
+
     });
+
   }
 
-  clientInput.addEventListener("keyup", function (evt) {
+  clientInput.addEventListener("keyup", (evt) => {
 
     page = 1;
 
-    if (evt.keyCode === 13) {
+    if (evt.keyCode === 13){
       getData();
       prevB.classList.add("grey");
       console.log(page);
     }
+
   });
 
-  nextB.addEventListener("click", function () {
+  nextB.addEventListener("click", () => {
 
-    if (page < totalCount / 30) {
+    if (page < (totalCount / 30)){
       prevB.classList.remove("grey");
       page++;
       getData();
-    } else if (page >= totalCount / 30) {
-      nextB.classList.add("grey");
+    } else if (page >= (totalCount / 30)){
+      nextB.classList.add("grey")
     }
+
   });
 
-  prevB.addEventListener("click", function () {
+  prevB.addEventListener("click", () => {
 
-    if (page > 1) {
+    if (page > 1){
       nextB.classList.remove("grey");
       page--;
       getData();
     }
+
   });
-})();
-//# sourceMappingURL=script.js.map
+
+})()
